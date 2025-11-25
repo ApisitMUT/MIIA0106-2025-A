c932e03a47ebbc79fc63bf44ff669475e5880949
ApisitMUT <6811130035@mut.ac.th>
2025-11-25 17:25:04 +0700

    การทดลองครั้งที่2

---
+++ lab02/Test 1.cpp
@@ -0,0 +1,15 @@
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "Hello lab02!\n";
+    std::cout << "Name: Apisit Panpunya" << std::endl;
+    std::cout << "Cours: MIIA0106" << std::endl;
+    std::cout << "Email: 6811130035@mut.ac.th" << std::endl;
+    return 0;
+   
+}
+
